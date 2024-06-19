25b4edf603d307ecd8ed030180c3397f98554e90
kooyesung <jh8ckr@gmail.com>
2024-06-20 01:09:56 +0900

    [Close #37] 로딩화면 구현

---
+++ src/components/common/Loding.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
+import styles from './Loding.module.css'
+
+const Loding = () => {
+
+    return(
+        <div className={styles.overlay}>
+            <div className={styles.loding}>
+                <Oval
+            color='#FFFFFF'
+            width={250}
+            height={250}
+            />    
+            <img src='./images/etc/Vector-3.png' className={styles.img}></img>
+            <div className={styles.textBox}>
+                <p className={styles.text}>잠시만 기다려주세요...</p>
+                <p className={styles.text}>OO을 위한 추천 사료가 준비되고있습니다!</p>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Loding;
