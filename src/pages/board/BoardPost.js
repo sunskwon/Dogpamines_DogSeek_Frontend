6f533b7736e466625a14b31c899d49223ce4be5a
박진현 <emem2080@naver.com>
2024-06-21 18:10:28 +0900

    [FEATURE][BOARD] css 작업

--- src/pages/board/BoardPost.js
+++ src/pages/board/BoardPost.js
@@ -1,6 +1,9 @@
 import styles from "./BoardPost.module.css";
 
+
 function Post(){
+
+
 
     return(
         <div className={styles.display_post}>
@@ -19,27 +22,34 @@ function Post(){
         </div>
         </div>
         <hr color="D4D4D4"/>
-
-        <div>
+        <div className={styles.hr_post}>
             게시글 내용 제가 최근에 최근을 넘어서 최근에 화재가 되고 있는걸 해보았습니다...
         </div>
         <hr color="D4D4D4"/>
 
         <div>
-            <div>
-                <img src="/images/board/siren.svg"/>
-                <span>신고하기</span>
-            </div>
+            <div className={styles.button_siren}>
                 <button className={styles.button_delete}>삭제</button>
+                <span className={styles.siren_icon}>
+                    <img src="/images/board/siren.svg" />
+                    <span>신고하기</span>
+                </span>
+            </div>
+        </div>
+        <div>
+            <p className={styles.comment_title}>댓글</p>
+            <span className={styles.comment_title}>점심먹고땡
+                <span>2024.06.03</span>
+            <p className={styles.comment_title}>한쿡말은 잘모르지만 쿠냥 마냥 좋은 갱얼쥐 good lol</p>
+            </span>
+            <div>
+                
+            </div>
         </div>
 
-        <p>댓글</p>
-        <p>닉네임</p>
-        <p>글내용</p>
-        
-        <div>
-            <input ></input>
-            <button>등록</button>
+        <div className={styles.comment_wrap}>
+            <input type="text" placeholder="댓글을 입력해주세요" className={styles.comment_box}/>
+            <button className={styles.comment_button}>등록</button>
         </div>
         </div>
         
