cd4f9344dac6f54f4823ed5e83d1163f2f868c30
박진현 <emem2080@naver.com>
2024-06-24 17:14:24 +0900

    [#45] view all board page key

--- src/pages/board/Board.js
+++ src/pages/board/Board.js
@@ -1,12 +1,15 @@
-import { useState  } from 'react';
+import { useState, useEffect  } from 'react';
 import styles from "./Board.module.css";
 import { useNavigate } from 'react-router-dom';
+import { GetAPI } from '../../api/RestAPIs';
 
 function Board(){
-
     const [isOneNoticeOpen, setIsOneNoticeOpen] = useState(false);
     const [isTwoNoiceOpen, setIsTwoNoiceOpen] = useState(false);
     const [isThreeNoiceOpen, setIsThreeNoiceOpen] = useState(false);
+    const [boards, setBoards] = useState([]);
+
+
 
     const toggleOneModal = () => {
         setIsOneNoticeOpen(prevState => !prevState);
@@ -24,10 +27,31 @@ function Board(){
         navigate("./BoardPost");
     }
 
+    
+const call = async () => {
+
+    const address = '/boards'
+
+    const response = await GetAPI(address);
+
+    const result = await response.boards;  
+
+    return result;
+};
+
+    useEffect(() => {
+        call().then(res => setBoards(res));
+    }, []);
+    console.log(boards)
+
+
+
 
 
     return(
-        <div>
+        <>
+        {boards.map(boards => (
+        <div key={boards.postCode}>
             <div className={styles.board_dogImg }>
                 <img src={"/images/board/dogsa.png"}/>
             </div>
@@ -41,37 +65,37 @@ function Board(){
         <div>
         <hr color="D4D4D4"/>
             <div>
-                <button className={styles.notice_toggle} onClick={toggleOneModal}>
+            <button className={styles.notice_toggle} onClick={toggleOneModal}>
                 {isOneNoticeOpen ? '-' : '+'}
-                </button>
-            <span className={styles.notice_head}>공지 제목</span>
+                <span className={styles.notice_head}>{boards.postTitle}</span>
+            </button>
         </div>
             {isOneNoticeOpen &&
             <div className={styles.notice_content}>
-                공지 내용입니다.
+                {boards.postContext}
             </div>}
         </div>
             <hr color="D4D4D4"/>
             <div>
                 <button className={styles.notice_toggle} onClick={toggleTwoModal}>
                     {isTwoNoiceOpen ? '-' : '+'}
+                    <span className={styles.notice_head}>{boards.postTitle}</span>
                 </button>
-            <span className={styles.notice_head}>두 번째 공지입니다.</span>
         </div>
             {isTwoNoiceOpen &&
             <div className={styles.notice_content}>
-                두 번째 공지는 직접확인해보시기 바랍니다.
+                {boards.postContext}
             </div>}
         <hr color="D4D4D4"/>
         <div>
                 <button className={styles.notice_toggle} onClick={toggleThreeModal}>
                     {isThreeNoiceOpen ? '-' : '+'}
+                    <span className={styles.notice_head}>{boards.postTitle}</span>
                 </button>
-            <span className={styles.notice_head}>세 번째 공지입니다.</span>
         </div>
             {isThreeNoiceOpen &&
             <div className={styles.notice_content}>
-                첫 번째 공지는 그냥 공지가 아닙니다 알아 두셔야하는 공지입니다.
+                {boards.postContext}
             </div>}
         <hr color="D4D4D4"/>
     </div>
@@ -90,20 +114,24 @@ function Board(){
     <div className={styles.boardboxlines}>
         <div className={styles.boardbox}>
             <ul className={styles.boardboxTitle}>
-                <p className={styles.titletext}>오늘의 댕사이</p>
+                <p className={styles.titletext}>{boards.postTitle}</p>
             </ul>
                 <p className={styles.boxTitle}>우리 댕댕이는 안물지도 ,안물기는 무슨 오늘도 물림;</p>            
-                <p className={styles.nick}>포도맛폴라포</p>
+                <div className={styles.nick_interval}>
+                    <span className={styles.nick}>{boards.userNick}</span>
+                    <span className={styles.post_date}>{boards.postDate}</span>
+                </div>
             </div>
 
             <div className={styles.boardbox}>
             <ul className={styles.boardboxTitle}>
                     <p className={styles.titletext}>오늘의 댕사이</p>
                 </ul>
-                <p className={styles.boxTitle}>오늘도 밥 맛있게 먹은 우리 강쥐 귀여워</p>            
-                <span className={styles.nick}>오동통댕댕묘
-                    <p>2024.06.03</p>
-                </span>
+                    <p className={styles.boxTitle}>오늘도 밥 맛있게 먹은 우리 강쥐 귀여워</p>            
+                <div className={styles.nick_interval}>
+                    <span className={styles.nick}>{boards.userNick}</span>
+                    <span className={styles.post_date}>{boards.postDate}</span>
+                </div>
             </div>
 
             <div className={styles.boardbox}>
@@ -111,7 +139,10 @@ function Board(){
                     <p className={styles.titletext}>오늘의 댕사이</p>
                 </ul>
                 <p className={styles.boxTitle}>맨날 우리 강쥐는 누구쒜요 하면서 쳐다보는데 커욥..</p>            
-                <p className={styles.nick}>내이름은고난했죠</p>
+                <div className={styles.nick_interval}>
+                    <span className={styles.nick}>내이름고난했죠</span>
+                    <span className={styles.post_date}>2024.06.03</span>
+                </div>
             </div>
 
             <div className={styles.boardbox}>
@@ -119,15 +150,19 @@ function Board(){
                     <p className={styles.titletext}>오늘의 댕사이</p>
                 </ul>
                 <p className={styles.boxTitle}>오늘 하루종일 뛰다가 지쳤는지 새근새근 잠듦..ㅎㅎ</p>            
-                <p className={styles.nick}>타로타로</p>
+                <div className={styles.nick_interval}>
+                    <span className={styles.nick}>타로타로</span>
+                    <span className={styles.post_date}>2024.06.03</span>
+                </div>
             <button onClick={onBoardClick}>
                 게시물
             </button>
         </div>
     </div>
 
-                    
-        </div>
+</div>
+    ))}
+    </>
     );
 }
 
