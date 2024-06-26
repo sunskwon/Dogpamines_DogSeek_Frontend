7dfb6c02516ae42a65593d5236627eafc792b3d5
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-06-26 17:21:37 +0900

    [#79] Curation List API

--- src/pages/mypage/Mydog.js
+++ src/pages/mypage/Mydog.js
@@ -1,35 +1,66 @@
 import styles from './Mydog.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useState, useRef, useEffect } from "react";
 import { GetAPI } from '../../api/RestAPIs';
 
 function Mydog(){
 
+        const navigate = useNavigate();
+        const location = useLocation();
+
+        const userCode = 1;
+        
         const [modalOpen, setModalOpen] = useState(false);
         const modalBackground = useRef();
 
-        const userCode = 1;
         const [curations, setCurations] = useState([]);
-    
-    
-        const userCurationList = async () => {
-    
-            const address = `/curations?userCode=${userCode}`;
-    
-            const response = await GetAPI(address, userCode);
-    
-            const result = await response.curations;
-    
-            return result;
-        }
+        const [curationsDog, setCurationsDog] = useState([]);
+        const [curationCode, setCurationCode] = useState([]);
+        const [curationName, setCurationName] = useState('');
 
+        const fetchCurations = async () => {
+            const curationsAddress = `/curations?userCode=${userCode}`;
+            const curationsResponse = await GetAPI(curationsAddress);
+            setCurations(curationsResponse.curations);
+
+            if (curationsResponse.curations.length > 0) {
+                setCurationName(curationsResponse.curations[0].curationName);
+            }
+
+        };
+           
+        const fetchCurationsDog = async (name) => {
+
+            const curationsDogAddress = `/curationsDog?userCode=${userCode}&curationCode=${curationCode}&curationName=${name}`;
+            const curationsDogResponse = await GetAPI(curationsDogAddress);
+            setCurationsDog(curationsDogResponse.curationsDog);
+            
+        };
+
+        console.log(userCode);
+        console.log(curationName);
+        console.log(curationCode);
+    
         useEffect(() => {
-            userCurationList().then(res =>
+            fetchCurations();
+        }, [userCode]);
     
-                setCurations(res)
-            );
-        }, []);
+        useEffect(() => {
+            if (curationName) {
+                fetchCurationsDog(curationName);
+            }
+        }, [curationName]);
     
+        const onClick = (name) => {
+            setCurationName(name);
+            navigate('/mydog', {
+                state: {
+                    userCode: userCode,
+                    curationCode: curationCode,
+                    curationName: name
+                }
+            });
+        };
 
     return(
         <>
@@ -46,7 +77,7 @@ function Mydog(){
                     <span className={styles.text5}>반려견 정보</span>
             {curations.map (curation => (
                     <div className={styles.linkContainer} key={curation.userCode}>
-                       <button className={styles.text4}>{curation.curationName}</button>
+                       <button className={styles.text4} name={curation.curationName} onClick={() => onClick(curation.curationName)}>{curation.curationName}</button>
                     </div>
                 ))}
                     <Link to={'/curation'} className={styles.text4}>+ 반려견 추가 등록</Link>
@@ -60,10 +91,10 @@ function Mydog(){
                     <span className={styles.span1}>맞춤사료</span>
                 </div>
                     <hr className={styles.line4}/>
-                    {curations.map (curation => (
-                        <div className={styles.resultBox} key={curation.userCode}>
-                            <span className={styles.span2}>{curation.curationName}</span>
-                            <span className={styles.span3}>{curation.curationDate}</span>
+                    {curationsDog.map (curationDog => (
+                        <div className={styles.resultBox} key={curationDog.userCode}>
+                            <span className={styles.span2}>{curationDog.curationName}</span>
+                            <span className={styles.span3}>{curationDog.curationDate}</span>
                             <div className={styles.btnWrapper}>
                                 <button className={styles.btn2} onClick={() => setModalOpen(true)}>상세보기</button>
                             </div>
@@ -76,32 +107,62 @@ function Mydog(){
                                         }}>
                                             <div className={styles.modalContent}>
                                                 <div className={styles.modalTextContainer}>
-                                                    <p className={styles.modalText1}>{`회원님 반려견 ${curation.curationName}의 정보입니다.`}</p>
+                                                    <p className={styles.modalText1}>{`회원님 반려견 ${curationDog.curationName}의 정보입니다.`}</p>
                                                     <hr/>
                                                 </div>
                                                 <div className={styles.modalTextContainer2}>
                                                     <div className={styles.spanBox1}>
-                                                        <p className={styles.text6}>이름</p>
-                                                        <p className={styles.text6}>견종</p>
-                                                        <p className={styles.text6}>나이</p>
-                                                        <p className={styles.text6}>체형</p>
-                                                        <p className={styles.text6}>중성화 여부</p>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text6}>이름</span>
+                                                            <span className={styles.text8}>{curationDog.curationName}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text6}>견종</span>
+                                                            <span className={styles.text8}>{curationDog.curationBreed}</span>
+                                                        </div>
+                                                        <div  className={styles.spanBox2}>
+                                                            <span className={styles.text6}>나이</span>
+                                                            <span className={styles.text8}>{curationDog.curationAge}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text6}>체형</span>
+                                                            <span className={styles.text8}>{curationDog.curationSize}</span>
+                                                        </div>
+                                                        <div  className={styles.spanBox2}>
+                                                            <span className={styles.text6}>중성화 여부</span>
+                                                            <span className={styles.text8}>{curationDog.curationNeut}</span>
+                                                        </div>
                                                     </div>
                                                     <hr className={styles.modalLine1}/>
                                                     <div className={styles.spanBox1}>
-                                                        <p className={styles.text7}>몸무게</p>
-                                                        <p className={styles.text7}>질병 여부</p>
-                                                        <p className={styles.text7}>알러지 여부</p>
-                                                        <p className={styles.text7}>선호 식재료</p>
-                                                        <p className={styles.text7}>선호 조리방식</p>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text7}>몸무게</span>
+                                                            <span className={styles.text9}>{curationDog.curationWeight}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text7}>질병 여부</span>
+                                                            <span className={styles.text9}>{curationDog.curationDisease}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text7}>알러지 여부</span>
+                                                            <span className={styles.text9}>{curationDog.curationAllergy}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text7}>선호 식재료</span>
+                                                            <span className={styles.text9}>{curationDog.curationIngra}</span>
+                                                        </div>
+                                                        <div className={styles.spanBox2}>
+                                                            <span className={styles.text7}>선호 조리방식</span>
+                                                            <span className={styles.text9}>{curationDog.curationCook}</span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <button className={styles.modalCloseBtn} onClick={() => setModalOpen(false)}>닫기</button>
                                             </div>
                                     </div>}
-                            <button className={styles.btn1}>맞춤사료</button>
-                        </div>
-                    ))}
+                                <button className={styles.btn1}>맞춤사료</button>
+                            </div>
+                        ))}
             </div>
         </div>
         </>
