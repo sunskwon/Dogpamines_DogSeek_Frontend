a650845875816e3de027fdbab73720aaf864d295
kooyesung <jh8ckr@gmail.com>
2024-06-27 15:35:34 +0900

    [#80] Product Detail

--- src/components/common/Loding.js
+++ src/components/common/Loding.js
@@ -15,7 +15,7 @@ const Loding = () => {
             <img src='./images/etc/Vector-3.png' className={styles.img}></img>
             <div className={styles.textBox}>
                 <p className={styles.text}>잠시만 기다려주세요...</p>
-                <p className={styles.text}>OO을 위한 추천 사료가 준비되고있습니다!</p>
+                <p className={styles.text}>추천 사료가 준비되고있습니다!</p>
             </div>
             </div>
         </div>
