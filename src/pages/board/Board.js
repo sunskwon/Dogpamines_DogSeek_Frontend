1c87f4684966f525d53ffe138fadd35f6c60fc70
박진현 <emem2080@naver.com>
2024-07-03 18:16:54 +0900

    feature/boardF6 [#] 글작성

--- src/pages/board/Board.js
+++ src/pages/board/Board.js
@@ -1,38 +1,46 @@
-import { useState, useEffect } from 'react';
-import styles from "./Board.module.css";
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './Board.module.css';
 import { useNavigate } from 'react-router-dom';
-import { GetAPI } from '../../api/RestAPIs';
+import { GetAPI, PostAPI } from '../../api/RestAPIs';
+import { jwtDecode } from 'jwt-decode';
 
 function Board() {
-    const [isOneNoticeOpen, setIsOneNoticeOpen] = useState(false);
+    const date = new Date();
+    const today = `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)}-${date.getDate()}`;
+    const currentTime = `${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
+
+    const [inputText, setInputText] = useState("");
+    const activeButton = () => {
+        alert(`${inputText}입력 완료`);
+    }
+    const activeEnter = (e) => {
+        if(e.key === "Enter") {
+            activeButton();
+        }
+    }
+    const [isOneNoticeOpen, setIsOneNoticeOpen] = useState(false); 
     const [isTwoNoticeOpen, setIsTwoNoticeOpen] = useState(false);
     const [isThreeNoticeOpen, setIsThreeNoticeOpen] = useState(false);
-    const [notices, setNotices] = useState([]); // 공지사항 데이터
-    const [posts, setPosts] = useState([]); // 게시글 데이터
-    
-    const [loaded, setLoaded] = useState(false); // 데이터 로드 상태
+    const [notices, setNotices] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [newPost, setNewPost] = useState('');
+    const [loaded, setLoaded] = useState(false); 
 
-    const toggleOneModal = () => {
-        setIsOneNoticeOpen(prevState => !prevState);
-    };
-    const toggleTwoModal = () => {
-        setIsTwoNoticeOpen(prevState => !prevState);
-    };
-    const toggleThreeModal = () => {
-        setIsThreeNoticeOpen(prevState => !prevState);
-    };
+    const messagesEndRef = useRef(null);
+
+    const decodedToken = jwtDecode(window.localStorage.getItem("accessToken"));
+
+    const userCode = decodedToken.userCode;
+    const userNick = decodedToken.userNick;
+    const userAuth = decodedToken.userAuth;
+
+
+    const toggleOneModal = () => setIsOneNoticeOpen(prevState => !prevState);
+    const toggleTwoModal = () => setIsTwoNoticeOpen(prevState => !prevState);
+    const toggleThreeModal = () => setIsThreeNoticeOpen(prevState => !prevState);
 
     const navigate = useNavigate();
-
-    const onBoardClick = () => {
-        navigate("./BoardPost");
-    };
-    const onPostWritingClick = () => {
-        navigate("./BoardWriting");
-    };
-    const onKakaoClick = () => {
-        navigate("./BoardKakao");
-    };
+   // const onPostWritingClick = () => navigate("./BoardWriting");
 
     const call = async () => {
         const address = '/boards';
@@ -41,15 +49,37 @@ function Board() {
         return result;
     };
 
+    const inhandler = async () => {
+        const address = '/boards';
+        const newPostData = {
+            postContext: newPost,
+            postCategory: "자유",
+            postTime: currentTime,
+            postStatus: 'Y',
+            userCode: userCode, // 유저 코드 설정
+            userNick: userNick // 유저 닉네임 설정
+        };
+        try{
+            const response = await PostAPI(address, newPostData); // post 요청 보냄
+            if(response) {
+            setPosts([...posts, newPostData]); // 성공 시 클라이언트 측 상태 업로드
+            setNewPost(''); // 입력필드 초기화
+            scrollToBottom(); // 스크롤 맨 아래 이동
+        }
+    } catch (error) {
+        console.error('Error posting new data', error);
+    }
+}
+
     useEffect(() => {
         if (!loaded) {
             call().then(res => {
-                // 분리된 데이터 세팅
                 const noticeData = res.filter(post => post.postCategory === '공지');
                 const postData = res.filter(post => post.postCategory === '자유');
                 setNotices(noticeData);
                 setPosts(postData);
                 setLoaded(true);
+                scrollToBottom();
             }).catch(error => {
                 console.error('Error fetching data:', error);
                 setNotices([]);
@@ -58,84 +88,95 @@ function Board() {
         }
     }, [loaded]);
 
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
     return (
-        <>
-            <div>
-                <div className={styles.board_dogImg}>
-                    <img src={"/images/board/dogsa.png"} alt="Dog" />
+        <div className={styles.board_container}>
+            <div className={styles.board_dogImg}>
+                <img src={"/images/board/dogsa.png"} alt="Dog" />
+            </div>
+            <div className={styles.notice_all}>
+                <div>
+                    <p className={styles.noticeTitle}>공지사항</p>
                 </div>
-                <div className={styles.notice_all}>
-                    <div>
-                        <p className={styles.noticeTitle}>공지사항</p>
-                    </div>
-                </div>
+            </div>
 
-                <div className={styles.toggle}>
-                    <div>
-                        <hr color="D4D4D4" />
-                        <div>
-                            <button className={styles.notice_toggle} onClick={toggleOneModal}>
-                                {isOneNoticeOpen ? '-' : '+'}
-                                <span className={styles.notice_head}>{notices[0]?.postTitle}</span>
-                            </button>
-                        </div>
-                        {isOneNoticeOpen && notices[0] &&
-                            <div className={styles.notice_content}>
-                                {notices[0].postContext}
-                            </div>}
-                    </div>
+            <div className={styles.toggle}>
+                <div>
                     <hr color="D4D4D4" />
                     <div>
-                        <button className={styles.notice_toggle} onClick={toggleTwoModal}>
-                            {isTwoNoticeOpen ? '-' : '+'}
-                            <span className={styles.notice_head}>{notices[1]?.postTitle}</span>
+                        <button className={styles.notice_toggle} onClick={toggleOneModal}>
+                            {isOneNoticeOpen ? '-' : '+'}
+                            <span className={styles.notice_head}>{notices[0]?.postTitle}</span>
                         </button>
                     </div>
-                    {isTwoNoticeOpen && notices[1] &&
+                    {isOneNoticeOpen && notices[0] &&
                         <div className={styles.notice_content}>
-                            {notices[1].postContext}
+                            {notices[0].postContext}
                         </div>}
-                    <hr color="D4D4D4" />
-                    <div>
-                        <button className={styles.notice_toggle} onClick={toggleThreeModal}>
-                            {isThreeNoticeOpen ? '-' : '+'}
-                            <span className={styles.notice_head}>{notices[2]?.postTitle}</span>
-                        </button>
-                    </div>
-                    {isThreeNoticeOpen && notices[2] &&
-                        <div className={styles.notice_content}>
-                            {notices[2].postContext}
-                        </div>}
-                    <hr color="D4D4D4" />
                 </div>
-
-                <div className={styles.board_title}>
-                    <p>게시판</p>
-                    <div className={styles.search_box}>
-                        <input type="search" className={styles.search_input} name="keyword" placeholder="검색어를 입력하세요" />
-                        <button className={styles.button_writing} onClick={onPostWritingClick}>글쓰기</button>
-                        <img className={styles.search_icon} src='/images/board/Search.svg' alt="Search" />
-                    </div>
+                <hr color="D4D4D4" />
+                <div>
+                    <button className={styles.notice_toggle} onClick={toggleTwoModal}>
+                        {isTwoNoticeOpen ? '-' : '+'}
+                        <span className={styles.notice_head}>{notices[1]?.postTitle}</span>
+                    </button>
                 </div>
+                {isTwoNoticeOpen && notices[1] &&
+                    <div className={styles.notice_content}>
+                        {notices[1].postContext}
+                    </div>}
+                <hr color="D4D4D4" />
+                <div>
+                    <button className={styles.notice_toggle} onClick={toggleThreeModal}>
+                        {isThreeNoticeOpen ? '-' : '+'}
+                        <span className={styles.notice_head}>{notices[2]?.postTitle}</span>
+                    </button>
+                </div>
+                {isThreeNoticeOpen && notices[2] &&
+                    <div className={styles.notice_content}>
+                        {notices[2].postContext}
+                    </div>}
+                <hr color="D4D4D4" />
+            </div>
 
-                <div className={styles.boardboxlines}>
-                    {posts.slice(0, 5).map((post, index) => (
-                        <div className={styles.boardbox} key={index}>
-                            <div className={styles.boardboxTitle}>
-                                <span className={styles.titletext}>{post.postTitle}</span>
-                            </div>
-                            <p className={styles.boxTitle}>{post.postContext}</p>
-                            <div className={styles.nick_interval}>
-                                <span className={styles.nick}>{post.userNick}</span>
-                                <span className={styles.post_date}>{post.postDate}</span>
+            <div className={styles.board_title}>
+                <p>댕사이</p>
+                <div className={styles.search_box}>
+                </div>
+            </div>
+        
+            <div className={styles.container}>
+                <div className={styles.chat_wrap}>
+                    {posts.map((post, index) => (
+                        <div key={index} className={`${styles.chat_message} ${post.userNick === userNick ? styles.my_message : styles.other_message}`}>
+                            <span className={styles.talk_nick}>{post.userNick}</span>
+                            <div className={styles.chat}>
+                                <div className={styles.textbox}>
+                                    {post.postContext}
+                                </div>
+                                <span className={styles.talk_date}>{post.postDate} {post.postTime}</span>
                             </div>
                         </div>
                     ))}
-                    <button onClick={onBoardClick}>게시물</button>
-                    <button onClick={onKakaoClick}>2안</button>
+                    <div ref={messagesEndRef} />
                 </div>
             </div>
-        </>
+
+            <div className={styles.comment_wrap}>
+                <input 
+                    className={styles.comment_box} 
+                    maxLength='100' 
+                    placeholder='댓글을 입력해주세요 최대 100자까지 허용..'
+                    value={newPost}
+                    onChange={(e) => setNewPost(e.target.value)}
+                    onKeyDown={(e) => activeEnter(e)}
+                />
+                <button className={styles.comment_button}  onClick={inhandler}>등록</button>
+            </div>
+        </div>
     );
 }
 
--- src/pages/board/BoardKakao.js
+++ src/pages/board/BoardKakao.js
@@ -30,6 +30,9 @@ function Board() {
     const onPostWritingClick = () => {
         navigate("./BoardWriting");
     };
+    const onKakaoClick = () => {
+        navigate("./BoardKakao");
+    };
 
     const call = async () => {
         const address = '/boards';
@@ -56,7 +59,7 @@ function Board() {
     }, [loaded]);
 
     return (
-        
+        <>
             <div>
                 <div className={styles.board_dogImg}>
                     <img src={"/images/board/dogsa.png"} alt="Dog" />
@@ -107,44 +110,32 @@ function Board() {
                 </div>
 
                 <div className={styles.board_title}>
-                    <p>댕사이</p>
+                    <p>게시판</p>
                     <div className={styles.search_box}>
+                        <input type="search" className={styles.search_input} name="keyword" placeholder="검색어를 입력하세요" />
+                        <button className={styles.button_writing} onClick={onPostWritingClick}>글쓰기</button>
+                        <img className={styles.search_icon} src='/images/board/Search.svg' alt="Search" />
                     </div>
                 </div>
-<div>
-    <div className={styles.wrap}>
-        <div>
-            <span className={styles.talk_nick}>고무맛맛탕</span>
-            <div className={`${styles.chat} ${styles.ch1}`}>
-                <div className={styles.textbox}>
-                <span className={styles.textboxBefore}></span>
-                    안녕하세요 감사해요 잘있어요 다시 만나요
-                    어디로 가든지 할 수 가없 추ㅕㅜ요~~
-                    이쪽에서 저쪽에서 뭐든지같에 컬컬컬 콸콸콸ㅁㅁㅁㅁㅁㅁ
-                    각돌이가 오늘 산책 다녀와서도 지치지 않고 신나가지고 
-                    방방 뛰어 댕기다가 2시간만에 기절했네요 애가 
-                    에너지가 왜이리 좋은지 감당하기 힘들정도로 뛰어다닙니다...
-                    장난은 얌전히 치는데 그냥 뛰어노는게 즐거운가봐요 즐거운우리 각돌이 보기만 해도 귀엽네요 계속 그렇게만 자라줘!
-                </div>
-            <span className={styles.talk_date}>2024.02.06</span>
-        </div>
-        
-        <div>
-            <div className={`${styles.chat} ${styles.ch2}`} >
-            <div className={styles.textbox}>
-                <span className={styles.textboxBefore}>안녕하시고 반갑습니다 이번에 새로 개설하게된 댕사이 댕사료 큐레이션을 만들게된 사이트장 파트라dog 이라고 합니다. </span>
-            </div>
-            </div>
-        </div>
 
-    </div>
-    </div>
-        <div className={styles.comment_wrap}>
-            <input className={styles.comment_box} maxLength='100px' placeholder='댓글을 입력해주세요 최대 100자까지 허용..'></input>
-            <button className={styles.comment_button}>등록</button>
-        </div>
-</div>
-        </div>
+                <div className={styles.boardboxlines}>
+                    {posts.slice(0, 5).map((post, index) => (
+                        <div className={styles.boardbox} key={index}>
+                            <div className={styles.boardboxTitle}>
+                                <span className={styles.titletext}>{post.postTitle}</span>
+                            </div>
+                            <p className={styles.boxTitle}>{post.postContext}</p>
+                            <div className={styles.nick_interval}>
+                                <span className={styles.nick}>{post.userNick}</span>
+                                <span className={styles.post_date}>{post.postDate}</span>
+                            </div>
+                        </div>
+                    ))}
+                    <button onClick={onBoardClick}>게시물</button>
+                    <button onClick={onKakaoClick}>2안</button>
+                </div>
+            </div>
+        </>
     );
 }
 
