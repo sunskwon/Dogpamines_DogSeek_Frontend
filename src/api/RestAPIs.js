bc8acc4d133767f8e3fc85210f6f5cbea0775e0d
KANGMINSEO0 <juhee0629032@gmail.com>
2024-07-08 16:45:10 +0900

    [#166] Reissurance access token

--- src/api/RestAPIs.js
+++ src/api/RestAPIs.js
@@ -1,4 +1,24 @@
-export function GetAPI(address) {
+export async function GetAPI(address) {
+
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const url = `${baseUrl}${address}`;
+    const accessToken = await GetValidAccessToken();
+
+    return (
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Access-Cross-Allow-Origin': '*',
+                "Authorization": accessToken
+            },
+        }).then(res => res.json())
+    );
+};
+
+
+export function GetAPINotToken(address) {
 
     const baseUrl = process.env.REACT_APP_SPRING_SERVER;
     const url = `${baseUrl}${address}`;
@@ -10,25 +30,6 @@ export function GetAPI(address) {
                 'Content-Type': 'application/json',
                 'Accept': '*/*',
                 'Access-Cross-Allow-Origin': '*',
-                "Authorization": window.localStorage.getItem("accessToken")
-            },
-        }).then(res => res.json())
-    );
-};
-
-
-export function GetAPINotToken(address) {
-
-    const baseUrl = 'http://localhost:8080';
-    const url = `${baseUrl}${address}`;
-
-    return (
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*',
-                'Access-Cross-Allow-Origin': '*',
             },
         }).then(res => res.json())
     );
@@ -36,10 +37,11 @@ export function GetAPINotToken(address) {
 
 
 
-export function PostAPI(address, Object) {
+export async function PostAPI(address, Object) {
 
-    const baseUrl = process.env.REACT_APP_SPRING_SERVER;;
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
     const url = `${baseUrl}${address}`;
+    const accessToken = await GetValidAccessToken();
 
     return (
         fetch(url, {
@@ -48,17 +50,18 @@ export function PostAPI(address, Object) {
                 'Content-Type': 'application/json',
                 'Accept': '*/*',
                 'Access-Cross-Allow-Origin': '*',
-                "Authorization": window.localStorage.getItem("accessToken")
+                "Authorization": accessToken
             },
             body: JSON.stringify(Object),
         })
     );
 };
 
-export function PutAPI(address, Object) {
+export async function PutAPI(address, Object) {
 
-    const baseUrl = process.env.REACT_APP_SPRING_SERVER;;
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
     const url = `${baseUrl}${address}`;
+    const accessToken = await GetValidAccessToken();
 
     return (
         fetch(url, {
@@ -67,24 +70,25 @@ export function PutAPI(address, Object) {
                 'Content-Type': 'application/json',
                 'Accept': '*/*',
                 'Access-Cross-Allow-Origin': '*',
-                "Authorization": window.localStorage.getItem("accessToken")
+                "Authorization": accessToken
             },
             body: JSON.stringify(Object),
         })
     );
 };
 
-export function DeleteAPI(address) {
+export async function DeleteAPI(address) {
 
-    const baseUrl = process.env.REACT_APP_SPRING_SERVER;;
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
     const url = `${baseUrl}${address}`;
+    const accessToken = await GetValidAccessToken();
 
     return (
         fetch(url, {
             method: 'Delete',
             headers: {
                 'Access-Cross-Allow-Origin': '*',
-                "Authorization": window.localStorage.getItem("accessToken")
+                "Authorization": accessToken
             },
         })
     );
@@ -93,7 +97,8 @@ export function DeleteAPI(address) {
 // Login
 export const callLoginAPI = async ({ user }) => {
 
-    const requestURL = 'http://localhost:8080/login';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/login`;
 
     try {
         const response = await fetch(requestURL, {
@@ -134,7 +139,8 @@ export const callLoginAPI = async ({ user }) => {
 };
 
 export const checkAPI = async (check) => {
-    const requestURL = 'http://localhost:8080/user/check';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/user/check`;
 
     try {
         const response = await fetch(requestURL, {
@@ -167,7 +173,8 @@ export const checkAPI = async (check) => {
 // 회원가입
 export const callRegisterAPI = async ({ user }) => {
 
-    const requestURL = 'http://localhost:8080/signup';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/signup`;
 
     const requestBody = JSON.stringify({
         userId: user.userId,
@@ -196,9 +203,10 @@ export const callRegisterAPI = async ({ user }) => {
 }
 
 // 이메일 인증 코드 발송
-export const callEmailVerification = async ( email, type ) => {
+export const callEmailVerification = async (email, type) => {
 
-    const requestURL = 'http://localhost:8080/api/auth/send-verification-email';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/api/auth/send-verification-email`;
     const requestBody = JSON.stringify({
         email: email,
         type: type
@@ -225,9 +233,10 @@ export const callEmailVerification = async ( email, type ) => {
 }
 
 // 이메일 인증 확인
-export const callEmailVerify = async ( email, authNum ) => {
+export const callEmailVerify = async (email, authNum) => {
 
-    const requestURL = 'http://localhost:8080/api/auth/verify-email';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/api/auth/verify-email`;
     const requestBody = JSON.stringify({
         email: email,
         token: authNum
@@ -246,8 +255,7 @@ export const callEmailVerify = async ( email, authNum ) => {
 
     if (response.status === 200) {
         const result = response.headers.get("Result");
-            console.log(`q result : ${result}`);
-            return result;
+        return result;
     } else {
         const result = 'false';
         return result;
@@ -255,9 +263,10 @@ export const callEmailVerify = async ( email, authNum ) => {
 }
 
 // 사용자 ID 조회
-export const callFindUserId = async ( phone ) => {
+export const callFindUserId = async (phone) => {
 
-    const requestURL = 'http://localhost:8080/user/find/email';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/user/find/email`;
     const requestBody = JSON.stringify({
         phoneNumber: phone
     });
@@ -275,8 +284,8 @@ export const callFindUserId = async ( phone ) => {
 
     if (response.status === 200) {
         const result = response.headers.get("Result");
-            console.log(`q result : ${result}`);    // userId
-            return result;
+        console.log(`q result : ${result}`);    // userId
+        return result;
     } else {
         const result = 'false';
         return result;
@@ -284,16 +293,17 @@ export const callFindUserId = async ( phone ) => {
 }
 
 // 사용자 비밀번호 변경
-export const callChangePwd = async( userId, userPass ) => {
+export const callChangePwd = async (userId, userPass) => {
 
-    const requestURL = 'http://localhost:8080/user/change/pwd';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/user/change/pwd`;
     const requestBody = JSON.stringify({
         id: userId,
         pwd: userPass
     });
 
     const response = await fetch(requestURL, {
-        
+
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
@@ -302,7 +312,7 @@ export const callChangePwd = async( userId, userPass ) => {
         },
         body: requestBody
     });
-    
+
     if (response.status === 201) {
         const result = 'true';
         return result;
@@ -314,9 +324,11 @@ export const callChangePwd = async( userId, userPass ) => {
 }
 
 // 로그아웃
-export const callLogoutAPI = async() => {
+export const callLogoutAPI = async () => {
 
-    const requestURL = 'http://localhost:8080/auth/logout';
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/auth/logout`;
+    const accessToken = await GetValidAccessToken();
 
     const response = await fetch(requestURL, {
 
@@ -325,17 +337,89 @@ export const callLogoutAPI = async() => {
             "Content-Type": "application/json",
             "Accept": "*/*",
             'Access-Control-Allow-Origin': '*',
-            "Authorization": window.localStorage.getItem("accessToken"),
+            "Authorization": accessToken,
             "Refresh-Token": window.localStorage.getItem("refreshToken"),
         }
     });
 
     if (response.status === 200) {
         const result = 'true';
-            return result;
+        return result;
     } else {
         const result = 'false';
         return result;
+    }
+
+}
+
+// 토큰 만료 여부 확인하는 함수
+export function CheckTokenExpiration() {
+    const accessToken = window.localStorage.getItem("accessToken");
+    try {
+        const decodedToken = parseJwt(accessToken); // jwt-decode를 이용해 토큰을 디코딩
+        const exp = decodedToken.exp * 1000; // JWT의 exp는 초 단위이므로 밀리초로 변환
+
+        const currentTime = new Date().getTime();
+
+        return currentTime > exp; // 현재 시간이 만료 시간을 지났으면 true를 반환
+    } catch (error) {
+        return true; // JWT 디코딩 중 오류가 발생하면 만료된 것으로 간주
+    }
+}
+
+// Refresh Token을 사용하여 새로운 Access Token을 발급받는 함수
+export async function RefreshAccessToken() {
+
+    const baseUrl = process.env.REACT_APP_SPRING_SERVER;
+    const requestURL = `${baseUrl}/auth/refresh`;
+
+    const refreshToken = window.localStorage.getItem('refreshToken');
+    const accessToken = window.localStorage.getItem('accessToken');
+
+    const response = await fetch(requestURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'Access-Cross-Allow-Origin': '*',
+            "Authorization": accessToken,
+            'Refresh-Token': refreshToken
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Failed to refresh access token');
+    }
+    const jwtToken = response.headers.get("Authorization");
+
+    if (response.status === 200 && jwtToken) {
+        // 새로 발급된 Access Token을 localStorage에 저장
+        window.localStorage.setItem('accessToken', jwtToken);
+        return jwtToken;
+    } else {
+        return null;
+    }
+}
+
+// JWT를 디코딩하여 페이로드를 반환하는 함수
+export function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+// 유효한 Access Token을 반환하는 함수
+export async function GetValidAccessToken() {
+
+    if (CheckTokenExpiration()) {
+        const newAccessToken = await RefreshAccessToken();
+        return newAccessToken;
+    } else {
+        const accessToken = window.localStorage.getItem('accessToken');
+        return accessToken;
     }
 
 }
