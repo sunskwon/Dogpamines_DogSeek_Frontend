3b51d60da0931b460cf03122358756f7c8fc4a4f
eunseongjo <tkakfkekdk4@gmail.com>
2024-07-09 15:24:42 +0900

    dict commit

--- src/pages/dict/Dict.js
+++ src/pages/dict/Dict.js
@@ -1,34 +1,37 @@
 import styles from './Dict.module.css';
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
-import { GetAPI, GetAPINotToken } from "../../api/RestAPIs"
+import { GetAPINotToken } from "../../api/RestAPIs"
 
-function Dict(){
-    
-    const [isSmallModalOpen, setIsSmallModalOpen] = useState(false);
-    const [isMediumModalOpen, setIsMediumModalOpen] = useState(false);
-    const [isLargeModalOpen, setIsLargeModalOpen] = useState(false);
+function Dict() {
 
-    const toggleSmallModal = () => {
-        setIsSmallModalOpen(prevState => !prevState);
+    const [isSmallModalOpen, setIsSmallModalOpen] = useState(true);
+    const [isMediumModalOpen, setIsMediumModalOpen] = useState(true);
+    const [isLargeModalOpen, setIsLargeModalOpen] = useState(true);
+    const [isInSmallModalOpen, setInIsSmallModalOpen] = useState(true);
+    const [isInMediumModalOpen, setInIsMediumModalOpen] = useState(true);
+    const [isInLargeModalOpen, setInIsLargeModalOpen] = useState(true);
+
+    const toggleInSmallModal = () => {
+        setInIsSmallModalOpen(prevState => !prevState);
     };
-    const toggleMediumModal = () => {
-        setIsMediumModalOpen(prevState => !prevState);
+    const toggleInMediumModal = () => {
+        setInIsMediumModalOpen(prevState => !prevState);
     };
-    const toggleLargeModal = () => {
-        setIsLargeModalOpen(prevState => !prevState);
+    const toggleInLargeModal = () => {
+        setInIsLargeModalOpen(prevState => !prevState);
     };
 
     const [dogs, setDogs] = useState([]);
 
-    const [search, setSearch] = useState({dogName:''});
-        
+    const [search, setSearch] = useState({ dogName: '' });
+
     const selectAllDict = async () => {
-        
+
         const address = '/dict';
-        
+
         const response = await GetAPINotToken(address);
 
         const result = await response.dict;
@@ -56,33 +59,45 @@ function Dict(){
         setSearch({
             ...search,
             [name]: value
-            });
-    }; 
+        });
+    };
 
     const searchSubmitHandler = async (e) => {
-            e.preventDefault();
-            const results = await searchDict();
-            setIsSmallModalOpen(true);
-            setIsMediumModalOpen(true);
-            setIsLargeModalOpen(true);
-            console.log(search);
-            setDogs(results);
+        e.preventDefault();
+        const results = await searchDict();
+        setIsSmallModalOpen(true);
+        setIsMediumModalOpen(true);
+        setIsLargeModalOpen(true);
+        setInIsSmallModalOpen(true);
+        setInIsMediumModalOpen(true);
+        setInIsLargeModalOpen(true);
+        console.log(search);
+        setDogs(results);
     }
+
+    const allSearchHandler = e => {
+        const { name, value } = e.target;
+        setSearch({
+            ...search,
+            [name]: value
+        });
+    };
 
     const filterDogBySize = (size) => {
         return dogs.filter(dog => dog.dogSize === size);
     };
 
 
-    return(
+    return (
         <>
-        <div className={styles.container1}>
-            <div className={styles.title}>
-                <span className={styles.titletext1}> 견종에 대해 찾아보세요.</span>
-                <span className={styles.titletext2}>반려견 품종의 특징과 요구사항에 대한 전문적인 정보를 찾아보세요. <br/> 
-                스크롤 또는 검색 기능을 사용해 원하는 견종에 대한 정보를 찾을 수 있습니다. </span>
-                <img className={styles.img} src='https://lh3.google.com/u/0/d/1OYEelsFN-8xWWbi2SgMzZl3FN8fTx2mQ=w1920-h945-iv1'/>
-                <form className={styles.form} onSubmit={searchSubmitHandler}>
+            <div className={styles.container1}>
+                <div className={styles.title}>
+                    <span className={styles.titletext1}> 견종에 대해 찾아보세요.</span>
+                    <span className={styles.titletext2}>반려견 품종의 특징과 요구사항에 대한 전문적인 정보를 찾아보세요. <br />
+                        스크롤 또는 검색 기능을 사용해 원하는 견종에 대한 정보를 찾을 수 있습니다. </span>
+                    <img className={styles.img} src='https://lh3.google.com/u/0/d/1OYEelsFN-8xWWbi2SgMzZl3FN8fTx2mQ=w1920-h945-iv1' />
+                    <button className={styles.allSearch} onChange={allSearchHandler}>전체<br/>보기</button>
+                    <form className={styles.form} onSubmit={searchSubmitHandler}>
                         <input
                             className={styles.search}
                             type="text"
@@ -91,95 +106,107 @@ function Dict(){
                             value={search.dogName}
                             onChange={valueChangeHandler}
                         />
-                 <button className={styles.searchButton} type="submit">
-                    <img value={search.dogName} src='/images/dict/Search.png'/>
-                </button>
+                        <button className={styles.searchButton} type="submit">
+                            <img value={search.dogName} src='/images/dict/Search.png' />
+                        </button>
                     </form>
+                </div>
             </div>
-        </div>
- 
-        <div className={styles.container2} > 
-        <hr color="D4D4D4"/>
-            <button className={styles.size} onClick={toggleSmallModal}>
-                소형
-            </button>
 
-           {isSmallModalOpen && 
-            <div className={styles.grid} >
-              {filterDogBySize('소형견').length > 0 ? 
-                (
-                filterDogBySize('소형견').map((dog) => (
-                    <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{dogName: dog.dogName}}>
-                    <div className={styles.modalContainer}>
-                       <img className={styles.dogImages} src={dog.dogImage}/>
-                       <p className={styles.dogName}> {dog.dogName} </p>
+            <div className={styles.errorContainer} >
+                {filterDogBySize('소형견').length === 0 && filterDogBySize('중형견').length === 0 && filterDogBySize('대형견').length === 0 &&
+                    <>
+                        <img src='/images/animal/cuteDog.png' alt='error'/>
+                        <p>해당 내용이 포함된 견종이 없습니다.</p>
+                    </>
+                }
+            </div>
+
+            <div className={styles.container2} >
+
+                {isSmallModalOpen &&
+                    <div>
+                        {filterDogBySize('소형견').length > 0 &&
+                            (<>
+                                <hr color="D4D4D4" />
+                                <div className={styles.size} onClick={toggleInSmallModal}>
+                                    소형
+                                </div>
+                                {isInSmallModalOpen &&
+                                    <div className={styles.grid} >
+                                        {filterDogBySize('소형견').map((dog) => (
+                                            <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{ dogName: dog.dogName }}>
+                                                <div className={styles.modalContainer}>
+                                                    <img className={styles.dogImages} src={dog.dogImage} />
+                                                    <p className={styles.dogName}> {dog.dogName} </p>
+                                                </div>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                }
+                            </>
+                            )}
                     </div>
-                    </Link>
-                ))
-               ) : (
-                <>
-                <p></p><p className={styles.error}>해당 내용이 포함된 견종이 없습니다.</p>
-                </>
-               )}
-                </div>
                 }
 
-        </div>
-        <div className={styles.container2}>
-        <hr color="D4D4D4"/>
-            <button className={styles.size} onClick={toggleMediumModal}>
-                중형
-            </button>
-
-            {isMediumModalOpen &&
-            <div className={styles.grid}>
-                 {filterDogBySize('중형견').length > 0 ? 
-                  (  
-                    filterDogBySize('중형견').map((dog) => (
-                    <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{dogName: dog.dogName}}>
-                    <div className={styles.modalContainer} key={dog.dogCode}>
-                       <img className={styles.dogImages} src={dog.dogImage}/>
-                       <p className={styles.dogName}>{dog.dogName}</p> 
+            </div>
+            <div className={styles.container2}>
+                {isMediumModalOpen &&
+                    <div>
+                        {filterDogBySize('중형견').length > 0 &&
+                            (<>
+                                <hr color="D4D4D4" />
+                                <div className={styles.size} onClick={toggleInMediumModal}>
+                                    중형
+                                </div>
+                                {isInMediumModalOpen &&
+                                    <div className={styles.grid} >
+                                        {filterDogBySize('중형견').map((dog) => (
+                                            <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{ dogName: dog.dogName }}>
+                                                <div className={styles.modalContainer}>
+                                                    <img className={styles.dogImages} src={dog.dogImage} />
+                                                    <p className={styles.dogName}> {dog.dogName} </p>
+                                                </div>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                }
+                            </>
+                            )}
                     </div>
-                    </Link>
-                ))
-            ) : (
-                <>
-                <p></p><p className={styles.error}>해당 내용이 포함된 견종이 없습니다.</p>
-                </>
-            )}
-                </div>
                 }
 
-        </div>
-        <div className={styles.container2}>
-        <hr color="D4D4D4"/>
-            <button className={styles.size} onClick={toggleLargeModal}>
-                대형
-            </button>
 
-            {isLargeModalOpen &&
-            <div className={styles.grid}>
-                {filterDogBySize('대형견').length > 0 ? 
-                  (  
-                filterDogBySize('대형견').map((dog) => (
-                    <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{dogName: dog.dogName}}>
-                    <div className={styles.modalContainer} key={dog.dogCode}>
-                        <img className={styles.dogImages} src={dog.dogImage}/>
-                        <p className={styles.dogName}>{dog.dogName}</p> 
+            </div>
+            <div className={styles.container2}>
+                {isLargeModalOpen &&
+                    <div>
+                        {filterDogBySize('대형견').length > 0 &&
+                            (<>
+                                <hr color="D4D4D4" />
+                                <div className={styles.size} onClick={toggleInLargeModal}>
+                                    대형
+                                </div>
+                                {isInLargeModalOpen &&
+                                    <div className={styles.grid} >
+                                        {filterDogBySize('대형견').map((dog) => (
+                                            <Link to={`/dict/${dog.dogName}`} key={dog.dogName} state={{ dogName: dog.dogName }}>
+                                                <div className={styles.modalContainer}>
+                                                    <img className={styles.dogImages} src={dog.dogImage} />
+                                                    <p className={styles.dogName}> {dog.dogName} </p>
+                                                </div>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                }
+                            </>
+                            )}
                     </div>
-                    </Link>
-                ))
-            ) : (
-                <>
-                <p></p><p className={styles.error}>해당 내용이 포함된 견종이 없습니다.</p>
-                </>
-            )}
-                </div>
                 }
 
-        </div>
-    
+
+            </div>
+
         </>
     )
 }
