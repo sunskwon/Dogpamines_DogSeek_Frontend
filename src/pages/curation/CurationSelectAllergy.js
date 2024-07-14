0b4edc3235dd8576ea6ccc5e2092efacb9e4f421
kooyesung <jh8ckr@gmail.com>
2024-07-15 01:19:36 +0900

    [#208] CurationFix

--- src/pages/curation/CurationSelectAllergy.js
+++ src/pages/curation/CurationSelectAllergy.js
@@ -45,11 +45,44 @@ function CurationSelectAllergy() {
                 <select className={styles.inputSize} name='allergy' onChange={onAllergyChange}>
                     <option value={"none"}>선택</option>
                     <option value={"none"}>없음</option>
-                    <option>닭고기</option>
                     <option>소고기</option>
-                    <option>돼지고기</option>
                     <option>연어</option>
+                    <option>오리고기</option>
+                    <option>닭고기</option>
+                    <option>돼지고기</option>
+                    <option>양고기</option>
+                    <option>정어리</option>
+                    <option>사슴고기</option>
+                    <option>고등어</option>
+                    <option>칠면조</option>
+                    <option>등애등에분말</option>
+                    <option>밀웜</option>
+                    <option>감자</option>
+                    <option>고구마</option>
+                    <option>청어</option>
+                    <option>참치</option>
+                    <option>송어</option>
+                    <option>비트</option>
+                    <option>허브</option>
+                    <option>라즈베리</option>
+                    <option>블루베리</option>
+                    <option>당근</option>
+                    <option>호박</option>
+                    <option>코코넛</option>
+                    <option>석류</option>
+                    <option>사과</option>
+                    <option>계란</option>
+                    <option>토마토</option>
                     <option>콩</option>
+                    <option>녹두</option>
+                    <option>딸기</option>
+                    <option>치즈</option>
+                    <option>크렌베리</option>
+                    <option>망고</option>
+                    <option>자두</option>
+                    <option>바나나</option>
+                    <option>초록입홍합</option>
+                    <option>배</option>
                 </select>
                 <button type="submit" className={isButtonEnabled ? styles.nextButtonActive : styles.nextButton} disabled={!isButtonEnabled} onClick={onClick}>다음</button>
             </div>    
--- src/pages/curation/CurationSelectDisease.js
+++ src/pages/curation/CurationSelectDisease.js
@@ -35,7 +35,7 @@ function CurationSelectDisease() {
     return (
         <div>
             <div className={styles.mainBox}>
-                <p className={styles.curationText}>{name}은/는 어떤 질병및 질환이 있나요?</p>
+                <p className={styles.curationText}>{name}은/는 필요한 기능이 있나요?</p>
                 <div className={styles.barBox}>
                     <div className={styles.bar}></div>
                     <div>
@@ -46,11 +46,37 @@ function CurationSelectDisease() {
                 <select className={styles.inputSize} name='disease' onChange={onDiseaseChange}>
                     <option value={"none"}>선택</option>
                     <option value={"none"}>없음</option>
-                    <option>관절</option>
-                    <option>피부</option>
-                    <option>모질</option>
-                    <option>과체중</option>
-                    <option>면역</option>
+                    <option>저알러지</option>
+                    <option>면역력강화</option>
+                    <option>영양공급</option>
+                    <option>심장건강</option>
+                    <option>다이어트</option>
+                    <option>구강건강</option>
+                    <option>관절강화</option>
+                    <option>연골강화</option>
+                    <option>근육강화</option>
+                    <option>골격강화</option>
+                    <option>피모개선</option>
+                    <option>영양공급</option>
+                    <option>항산화</option>
+                    <option>요로</option>
+                    <option>비뇨</option>
+                    <option>식욕증진</option>
+                    <option>스트레스완화</option>
+                    <option>종합비타민</option>
+                    <option>활력증진</option>
+                    <option>기력회복</option>
+                    <option>근육발달</option>
+                    <option>음수량증진</option>
+                    <option>눈건강</option>
+                    <option>활력증진</option>
+                    <option>기력회복</option>
+                    <option>혈액건강</option>
+                    <option>노화방지</option>
+                    <option>혈액순환</option>
+                    <option>눈물자국</option>
+                    <option>운동효과증진</option>
+                    <option>성장발달</option>
                 </select>
                 <button type="submit" className={isButtonEnabled ? styles.nextButtonActive : styles.nextButton} disabled={!isButtonEnabled} onClick={onClick}>다음</button>
             </div>    
